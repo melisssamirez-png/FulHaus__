4b7c3fa9f6d036992ccd193ebf115e99caad1220
melisssamirez-png <melisssamirez@gmail.com>
2025-09-15 22:31:23 +0000

    primeros cambios

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Crear las listas de imágenes, títulos y frases
 const imagenes = [
-  "../prediccciones/imagenes/guss.jpg",
-  "../prediccciones/imagenes/isa.jpg",
-  "../prediccciones/imagenes/manuel.jpg",
-  "../prediccciones/imagenes/tom.jpg",
-  "../prediccciones/imagenes/meli.jpg",
-  "../prediccciones/imagenes/todos.jpg",
+  "../FulHaus__/imagenes/guss.jpg",
+  "../FulHaus__/imagenes/isa.jpg",
+  "../FulHaus__/imagenes/manuel.jpg",
+  "../FulHaus__/imagenes/tom.jpg",
+  "../FulHaus__/imagenes/meli.jpg",
+  "../FulHaus__/imagenes/todos.jpg",
 ];
 
 const titulos = [
